9d09af67ca2cfb4b7065cefac0e0b013dce057da
AlexCatch <alex@alexcatchpoledev.me>
2019-05-05 19:44:19 +0100

    Initial Commit

---
+++ Ninsho/Ninsho.h
@@ -0,0 +1,19 @@
+//
+//  Ninsho.h
+//  Ninsho
+//
+//  Created by Alex Catchpole on 05/05/2019.
+//  Copyright © 2019 Alex Catchpole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ninsho.
+FOUNDATION_EXPORT double NinshoVersionNumber;
+
+//! Project version string for Ninsho.
+FOUNDATION_EXPORT const unsigned char NinshoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ninsho/PublicHeader.h>
+
+
